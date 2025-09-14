3a83a913f1df2ef4e5cb2b89342df2c2727c37cf
Jogi Marvin Occiano <104772531+jogimarvin@users.noreply.github.com>
2025-09-14 18:39:09 +0800

    Update enable-winrm.ps1

--- scripts/enable-winrm.ps1
+++ scripts/enable-winrm.ps1
@@ -1,6 +1,23 @@
-# enable-winrm.ps1
+# === WINRM SETUP ===
+
 winrm quickconfig -quiet
 winrm set winrm/config/service/auth '@{Basic="true"}'
 winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 Enable-PSRemoting -Force
 Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP-PUBLIC" -RemoteAddress Any
+
+# === INSTALL ADDS ===
+Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
+Import-Module ADDSDeployment
+Install-ADDSForest `
+    -DomainName "rocku.com" `
+    -CreateDnsDelegation:$false `
+    -DatabasePath "C:\Windows\NTDS" `
+    -DomainMode "WinThreshold" `
+    -DomainNetbiosName "ROCKU" `
+    -ForestMode "WinThreshold" `
+    -InstallDns:$true `
+    -LogPath "C:\Windows\NTDS" `
+    -SysvolPath "C:\Windows\SYSVOL" `
+    -Force:$true `
+    -SafeModeAdministratorPassword (ConvertTo-SecureString "P@ssword1234!" -AsPlainText -Force)
