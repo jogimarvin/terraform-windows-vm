6d2f8822706c3a460b9b326a2092263d690c3d3f
Jogi Marvin Occiano <104772531+jogimarvin@users.noreply.github.com>
2025-09-14 21:07:58 +0800

    Update Install_adds.ps1

--- scripts/Install_adds.ps1
+++ scripts/Install_adds.ps1
@@ -1,23 +1,32 @@
-# === WINRM SETUP ===
+Import-Module ActiveDirectory
 
-winrm quickconfig -quiet
-winrm set winrm/config/service/auth '@{Basic="true"}'
-winrm set winrm/config/service '@{AllowUnencrypted="true"}'
-Enable-PSRemoting -Force
-Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP-PUBLIC" -RemoteAddress Any
+$users = Import-Csv "user_lifecycle.csv"
 
-# === INSTALL ADDS ===
-Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
-Import-Module ADDSDeployment
-Install-ADDSForest `
-    -DomainName "rocku.com" `
-    -CreateDnsDelegation:$false `
-    -DatabasePath "C:\Windows\NTDS" `
-    -DomainMode "WinThreshold" `
-    -DomainNetbiosName "ROCKU" `
-    -ForestMode "WinThreshold" `
-    -InstallDns:$true `
-    -LogPath "C:\Windows\NTDS" `
-    -SysvolPath "C:\Windows\SYSVOL" `
-    -Force:$true `
-    -SafeModeAdministratorPassword (ConvertTo-SecureString "P@ssword1234!" -AsPlainText -Force)
+foreach ($user in $users) {
+    switch ($user.Action) {
+        "Joiner" {
+            New-ADUser `
+                -Name "$($user.FirstName) $($user.LastName)" `
+                -SamAccountName $user.Username `
+                -UserPrincipalName "$($user.Username)@rocku.com" `
+                -GivenName $user.FirstName `
+                -Surname $user.LastName `
+                -Path $user.OU `
+                -AccountPassword (ConvertTo-SecureString $user.TempPassword -AsPlainText -Force) `
+                -Enabled $true
+            Write-Output "Created user: $($user.Username)"
+        }
+
+        "Mover" {
+            Move-ADObject `
+                -Identity "CN=$($user.FirstName) $($user.LastName),$user.OU" `
+                -TargetPath $user.NewOU
+            Write-Output "Moved user: $($user.Username)"
+        }
+
+        "Leaver" {
+            Disable-ADAccount -Identity $user.Username
+            Write-Output "Disabled user: $($user.Username)"
+        }
+    }
+}
