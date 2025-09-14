0815b815ca20e720370b5a42a1f9f94d474cf522
Jogi Marvin Occiano <104772531+jogimarvin@users.noreply.github.com>
2025-09-14 18:35:16 +0800

    Update Install_adds.ps1

--- scripts/Install_adds.ps1
+++ scripts/Install_adds.ps1
@@ -1,3 +1,12 @@
+# === WINRM SETUP ===
+
+winrm quickconfig -quiet
+winrm set winrm/config/service/auth '@{Basic="true"}'
+winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+Enable-PSRemoting -Force
+Set-NetFirewallRule -Name "WINRM-HTTP-In-TCP-PUBLIC" -RemoteAddress Any
+
+# === INSTALL ADDS ===
 Install-WindowsFeature AD-Domain-Services -IncludeManagementTools
 Import-Module ADDSDeployment
 Install-ADDSForest `
